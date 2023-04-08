513a32585f229ff60db4cb37331ac39473d3cd21
Joe Fogiato <joe.fogiato@gmail.com>
2023-04-07 20:23:32 -0400

    Add: Root CSS variables in App.css, initial styling for Feelings component, revision of Feelings component names & nesting structure

--- src/Components/Feelings/Feelings.js
+++ src/Components/Feelings/Feelings.js
@@ -8,29 +8,31 @@ const Feelings = ({ setFeeling }) => {
   }
 
   return (
-    <section className='feelings'>
-      <h2>How are you Feeling?</h2>
-      <button className="feel-btn happy" onClick={callBack}>Happy</button>
-      <button className="feel-btn happy" onClick={callBack}>Content</button>
-      <button className="feel-btn happy" onClick={callBack}>Optimistic</button>
-      <button className="feel-btn sad" onClick={callBack}>Sad</button>
-      <button className="feel-btn sad" onClick={callBack}>Lonely</button>
-      <button className="feel-btn sad" onClick={callBack}>Depressed</button>
-      <button className="feel-btn surprised" onClick={callBack}>Surpised</button>
-      <button className="feel-btn surprised" onClick={callBack}>Confused</button>
-      <button className="feel-btn surprised" onClick={callBack}>Excited</button>
-      <button className="feel-btn bad" onClick={callBack}>Bad</button>
-      <button className="feel-btn bad" onClick={callBack}>Stressed</button>
-      <button className="feel-btn bad" onClick={callBack}>Tired</button>
-      <button className="feel-btn fearful" onClick={callBack}>Fearful</button>
-      <button className="feel-btn fearful" onClick={callBack}>Anxious</button>
-      <button className="feel-btn fearful" onClick={callBack}>Insecure</button>
-      <button className="feel-btn angry" onClick={callBack}>Angry</button>
-      <button className="feel-btn angry" onClick={callBack}>Distant</button>
-      <button className="feel-btn angry" onClick={callBack}>Frustrated</button>
-      <button className="feel-btn disguisted" onClick={callBack}>Disguisted</button>
-      <button className="feel-btn disguisted" onClick={callBack}>Disappointed</button>
-      <button className="feel-btn disguisted" onClick={callBack}>Awful</button>
+    <section className='feelings-page'>
+      <h2 className='feelings-header'>How are you Feeling?</h2>
+      <section className='feelings-container'>
+        <p className='feeling happy' onClick={callBack}>Happy</p>
+        <p className='feeling happy' onClick={callBack}>Content</p>
+        <p className='feeling happy' onClick={callBack}>Optimistic</p>
+        <p className='feeling sad' onClick={callBack}>Sad</p>
+        <p className='feeling sad' onClick={callBack}>Lonely</p>
+        <p className='feeling sad' onClick={callBack}>Depressed</p>
+        <p className='feeling surprised' onClick={callBack}>Surpised</p>
+        <p className='feeling surprised' onClick={callBack}>Confused</p>
+        <p className='feeling surprised' onClick={callBack}>Excited</p>
+        <p className='feeling bad' onClick={callBack}>Bad</p>
+        <p className='feeling bad' onClick={callBack}>Stressed</p>
+        <p className='feeling bad' onClick={callBack}>Tired</p>
+        <p className='feeling fearful' onClick={callBack}>Fearful</p>
+        <p className='feeling fearful' onClick={callBack}>Anxious</p>
+        <p className='feeling fearful' onClick={callBack}>Insecure</p>
+        <p className='feeling angry' onClick={callBack}>Angry</p>
+        <p className='feeling angry' onClick={callBack}>Distant</p>
+        <p className='feeling angry' onClick={callBack}>Frustrated</p>
+        <p className='feeling disguisted' onClick={callBack}>Disguisted</p>
+        <p className='feeling disguisted' onClick={callBack}>Disappointed</p>
+        <p className='feeling disguisted' onClick={callBack}>Awful</p>
+      </section>
     </section>
   )
 }
