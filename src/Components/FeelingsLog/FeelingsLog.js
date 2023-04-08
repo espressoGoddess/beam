b7c959b177e705ce1378170066b9de99c9cd3d6a
Amber Shipley <ashipley2@me.com>
2023-04-07 17:38:48 -0700

    add: table and styling for feelings logs

--- src/Components/FeelingsLog/FeelingsLog.js
+++ src/Components/FeelingsLog/FeelingsLog.js
@@ -1,10 +1,36 @@
-import React from "react";
+import React from 'react';
+import './FeelingsLog.css';
 
-const FeelingsLog = () => {
+const toTitleCase= () => {
+  
+}
+
+const FeelingsLog = ({logs}) => { 
+  const  userLogs = logs.map(log => {
+      return (<tr key={log.entryId}>
+          <td>{log.date}</td>
+          <td>{log.feeling}</td>
+          <td>{log.activity}</td>
+          <td>{log.journalBool ? '📓' : null}</td>
+        </tr>)
+    })
   return (
-    <>
-    <h2>Feelings Log</h2>
-    </>
+    <div className='FeelingsLog'>
+      <table>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Feeling</th>
+            <th>Activity</th>
+            <th>Journal</th>
+          </tr>
+        </thead>
+        <tbody>
+        {userLogs}
+        </tbody>
+      </table>
+
+    </div>
   )
 }
 
