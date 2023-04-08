9e7b85962747dfaff1dbe8e8e9ca76bd71a7ce0d
Joe Fogiato <joe.fogiato@gmail.com>
2023-04-08 08:50:01 -0400

    Change: callback function name to a more semantic choice

--- src/Components/Feelings/Feelings.js
+++ src/Components/Feelings/Feelings.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import './Feelings.css'
+import './Feelings.css';
 
 const Feelings = ({ setFeeling }) => {
 
-  const callBack = ({ target }) => {
+  const selectFeeling = ({ target }) => {
     setFeeling(target.classList[1]);
   }
 
@@ -12,30 +12,30 @@ const Feelings = ({ setFeeling }) => {
       <h1>BEAM</h1>
       <h2 className='feelings-header'>How are you today?</h2>
       <section className='feelings-container'>
-        <p className='feeling happy' onClick={callBack}>Happy</p>
-        <p className='feeling happy' onClick={callBack}>Content</p>
-        <p className='feeling happy' onClick={callBack}>Optimistic</p>
-        <p className='feeling sad' onClick={callBack}>Sad</p>
-        <p className='feeling sad' onClick={callBack}>Lonely</p>
-        <p className='feeling sad' onClick={callBack}>Depressed</p>
-        <p className='feeling surprised' onClick={callBack}>Surpised</p>
-        <p className='feeling surprised' onClick={callBack}>Confused</p>
-        <p className='feeling surprised' onClick={callBack}>Excited</p>
-        <p className='feeling bad' onClick={callBack}>Bad</p>
-        <p className='feeling bad' onClick={callBack}>Stressed</p>
-        <p className='feeling bad' onClick={callBack}>Tired</p>
-        <p className='feeling fearful' onClick={callBack}>Fearful</p>
-        <p className='feeling fearful' onClick={callBack}>Anxious</p>
-        <p className='feeling fearful' onClick={callBack}>Insecure</p>
-        <p className='feeling angry' onClick={callBack}>Angry</p>
-        <p className='feeling angry' onClick={callBack}>Distant</p>
-        <p className='feeling angry' onClick={callBack}>Frustrated</p>
-        <p className='feeling disguisted' onClick={callBack}>Disguisted</p>
-        <p className='feeling disguisted' onClick={callBack}>Disappointed</p>
-        <p className='feeling disguisted' onClick={callBack}>Awful</p>
+        <p className='feeling happy' onClick={selectFeeling}>Happy</p>
+        <p className='feeling happy' onClick={selectFeeling}>Content</p>
+        <p className='feeling happy' onClick={selectFeeling}>Optimistic</p>
+        <p className='feeling sad' onClick={selectFeeling}>Sad</p>
+        <p className='feeling sad' onClick={selectFeeling}>Lonely</p>
+        <p className='feeling sad' onClick={selectFeeling}>Depressed</p>
+        <p className='feeling surprised' onClick={selectFeeling}>Surpised</p>
+        <p className='feeling surprised' onClick={selectFeeling}>Confused</p>
+        <p className='feeling surprised' onClick={selectFeeling}>Excited</p>
+        <p className='feeling bad' onClick={selectFeeling}>Bad</p>
+        <p className='feeling bad' onClick={selectFeeling}>Stressed</p>
+        <p className='feeling bad' onClick={selectFeeling}>Tired</p>
+        <p className='feeling fearful' onClick={selectFeeling}>Fearful</p>
+        <p className='feeling fearful' onClick={selectFeeling}>Anxious</p>
+        <p className='feeling fearful' onClick={selectFeeling}>Insecure</p>
+        <p className='feeling angry' onClick={selectFeeling}>Angry</p>
+        <p className='feeling angry' onClick={selectFeeling}>Distant</p>
+        <p className='feeling angry' onClick={selectFeeling}>Frustrated</p>
+        <p className='feeling disguisted' onClick={selectFeeling}>Disguisted</p>
+        <p className='feeling disguisted' onClick={selectFeeling}>Disappointed</p>
+        <p className='feeling disguisted' onClick={selectFeeling}>Awful</p>
       </section>
     </section>
-  )
+  );
 }
 
 export default Feelings;
