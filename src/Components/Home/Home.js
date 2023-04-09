bf2fd33b52dca84c6ea59692188589949282ed93
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-04-09 17:07:30 -0500

    refactor: Change to thanks for stopping by, since the button ends in 'in'

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <section className='home'>
       <h1 className='logo-home'>BEAM</h1>
-      <h2>Thanks for stopping in</h2>
+      <h2>Thanks for stopping by</h2>
       <Link to='/how-are-you-feeling' className='uni-btn'>Let's dig in.</Link>
     </section>
   );
