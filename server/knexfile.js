73621e56ccdb9f553b97437408070f5c1832f64e
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-04-10 18:31:20 -0500

    feat: Add first route to server.js

---
+++ server/knexfile.js
@@ -0,0 +1,50 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      port: 5432,
+      user: "chrissy",
+      password: null,
+      database: "beam_api"
+    }
+  },
+
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false
+      }
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+};
---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const knex = require('./knexfile');
+const port = 3001;
+
+app.set('port', port);
+
+app.use(cors());
+app.use(express.json());
+
+
+app.listen(port, () => {
+  console.log(`Server has started on port ${port} 💸 🔦 💫`);
+});
+
+// ROUTES
+
+app.get('/api/v1/entries', async (req, res) => {
+  try {
+    const entries = await knex.select().from("entries");
+    res.status(200).json(entries);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
