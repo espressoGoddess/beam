d8ccb9e526a0b003ea20788bfef5d484d44d85d0
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-04-10 19:23:48 -0500

    feat: Create our first migrationgit statusgit status

---
+++ server/migrations/20230410234816_init.js
@@ -0,0 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('entries', (table) => {
+    table.increments('entry_id')
+    table.string('user_id')
+    table.string('date')
+    table.string('feeling')
+    table.string('activity')
+    table.string('journal_entry', 10000)
+    table.timestamps()
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable('entries');
+};
