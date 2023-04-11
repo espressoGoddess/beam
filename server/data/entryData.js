525c8368b4cecc915324f7c806afb247d51ada74
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-04-10 19:50:15 -0500

    feat: Create backend data file and Seed our first tablegit add .git add .

---
+++ server/data/entryData.js
@@ -0,0 +1,74 @@
+const entryData = [
+  {
+    "user_id": 1,
+    "date": "2023-04-07",
+    "feeling": "anxious",
+    "activity": "Go find a spider.",    
+    "journal_entry": "Today I am feeling anxoius because I pooped my pants in front of my crush."
+  },
+  {
+    "user_id": 1,
+    "date": "2023-04-06",
+    "feeling": "mad",
+    "activity": "Take a deep breath.",
+    "journal_entry": ""
+  },
+  {
+    "user_id": 1,
+    "date": "2023-04-05",
+    "feeling": "happy",
+    "activity": "Call a loved one and tell them you love them.",    
+    "journal_entry": "Today I am feeling anxoius because I pooped my pants in front of my crush."
+  },
+  {
+    "user_id": 1,
+    "date": "2023-04-04",
+    "feeling": "anxious",
+    "activity": "Cheese cheese cheese cheese cheese.",
+    "journal_entry": ""
+  },
+  {
+    "user_id": 1,
+    "date": "2023-04-03",
+    "feeling": "frustrated",
+    "activity": "Go find a spider.",    
+    "journal_entry": "Today I am feeling anxoius because I pooped my pants in front of my crush."
+  },
+  {
+    "user_id": 1,
+    "date": "2023-04-02",
+    "feeling": "nervous",
+    "activity": "Be cool.",
+    "journal_entry": ""
+  },
+  {
+    "user_id": 1,
+    "date": "2023-04-01",
+    "feeling": "jealous",
+    "activity": "Get over it.",    
+    "journal_entry": "Today I am feeling anxoius because I pooped my pants in front of my crush."
+  },
+  {
+    "user_id": 1,
+    "date": "2023-04-08",
+    "feeling": "friendly",
+    "activity": "Kiss a stranger on the mouth.",
+    "journal_entry": ""
+  },
+  {
+    "user_id": 1,
+    "date": "2023-04-09",
+    "feeling": "cheerful",
+    "activity": "Say your prayers.", 
+    "journal_entry": "Today I am feeling anxoius because I pooped my pants in front of my crush."
+  },
+  {
+    "user_id": 1,
+    "date": "2023-04-10",
+    "feeling": "happy",
+    "activity": "Be more sad.",
+    "journal_entry": ""
+  }
+]
+
+module.exports = entryData;
