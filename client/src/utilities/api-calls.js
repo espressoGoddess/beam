499f9cbc404bdf8bad8d261d58578979536b6ff3
Amber Shipley <ashipley2@me.com>
2023-04-12 16:11:30 -0700

    feat: Add post request to api calls file

--- client/src/utilities/api-calls.js
+++ client/src/utilities/api-calls.js
@@ -10,4 +10,20 @@ const getAllEntries = () => {
   .catch(error => console.log(error));
 }
 
-export { getAllEntries };
+const addNewEntry = (newEntry) => {
+  return fetch('http://localhost:3001/api/v1/entries', {
+    method: 'POST', 
+    headers: { 'Content-Type': 'application/json'},
+    body: JSON.stringify(newEntry)
+  })
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error(`HTTP Status Code: ${response.status}`);
+    }
+  })
+  .catch(error => console.log(error));
+}
+
+export { getAllEntries, addNewEntry };
