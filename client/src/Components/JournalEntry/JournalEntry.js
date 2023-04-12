6d4efff2c298e50e9c0d50da5471c208c3fbbb63
Katherine Blaine <blaine.katherine12@gmail.com>
2023-04-11 18:27:49 -0600

    feat: reformat journal entrypage

--- client/src/Components/JournalEntry/JournalEntry.js
+++ client/src/Components/JournalEntry/JournalEntry.js
@@ -10,11 +10,9 @@ const JournalEntry = ({ journal }) => {
   return (
     <section className='journal-boundary'>
       <div className='journal-entry'>
-        <h2>Journal Entry</h2>
+        <h2>{formattedDate.toLocaleString(DateTime.DATE_MED)}</h2>
         <div className='log-stats'>
-          <h3>{formattedDate.toLocaleString(DateTime.DATE_MED)}</h3>
-          <h3>I felt {feeling}</h3>
-          <h3>Activity I tried: {activity}</h3>
+          <h3>{`Today you were feeling ${feeling} so you decided to ${activity}`}</h3>
         </div>
         <div className='journal-content'>
           <p className='journal-text'>{journalEntry}</p>
