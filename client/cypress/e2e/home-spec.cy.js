aca7ca7ba5ee82522a357545db01e81420e1a472
Katherine Blaine <blaine.katherine12@gmail.com>
2023-04-13 15:27:11 -0600

    test: outline home spec DOM tests

--- client/cypress/e2e/home-spec.cy.js
+++ client/cypress/e2e/home-spec.cy.js
@@ -1,5 +1,11 @@
 describe('template spec', () => {
-  it('passes', () => {
+  it('Should display the site logo', () => {
+    cy.visit('https://example.cypress.io')
+  })
+  it('Should display the home blurb', () => {
+    cy.visit('https://example.cypress.io')
+  })
+  it('Should display a button that brings the user to the feelings page', () => {
     cy.visit('https://example.cypress.io')
   })
 })
