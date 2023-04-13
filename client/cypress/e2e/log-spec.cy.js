a471b949a314941800697b0ba587cebf2f938dd9
Katherine Blaine <blaine.katherine12@gmail.com>
2023-04-13 15:55:45 -0600

    test: outline feelings log spec DOM tests

--- client/cypress/e2e/log-spec.cy.js
+++ client/cypress/e2e/log-spec.cy.js
@@ -1,5 +1,18 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Feelings log page', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/how-you-felt')
+  })
+
+  it('Should display a table with columns for date, feeling, activity, and journal', () => {
     cy.visit('https://example.cypress.io')
   })
+
+  it('Should display the feeling and activity the user selected on the specified date within each table column ', () => {
+    cy.visit('https://example.cypress.io')
+  })
+
+  it('Should have a button in the journal column that brings the user from the feelings log page to the journal entry they completed on that specific day', () => {
+    cy.visit('https://example.cypress.io')
+  })
+
 })
