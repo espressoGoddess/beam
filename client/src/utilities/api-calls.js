3246130354f4db556d5796acfc73f820125630f1
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-04-13 19:16:51 -0500

    feat: Add updateEntry client side PUT call

--- client/src/utilities/api-calls.js
+++ client/src/utilities/api-calls.js
@@ -26,4 +26,21 @@ const addNewEntry = (newEntry) => {
   .catch(error => console.log(error));
 }
 
-export { getAllEntries, addNewEntry };
+const updateEntry = (updatedEntry, id) => {
+  return fetch(`http://localhost:3001/api/v1/entries/${id}`, {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({journal_entry: updatedEntry})
+  })
+  .then(response => {
+    if (response.ok) {
+      console.log('RESPONSE', response)
+      return response.json();
+    } else {
+      throw new Error(`HTTP Status Code: ${response.status}`);
+    }
+  })
+  .catch(error => console.log(error));
+}
+
+export { getAllEntries, addNewEntry, updateEntry };
