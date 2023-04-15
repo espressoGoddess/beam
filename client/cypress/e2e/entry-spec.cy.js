743e0d6ec235b07a0808cc44e97813e1c15ee7bc
Katherine Blaine <blaine.katherine12@gmail.com>
2023-04-14 18:04:13 -0600

    test: add initial testing for journal entry page

--- client/cypress/e2e/entry-spec.cy.js
+++ client/cypress/e2e/entry-spec.cy.js
@@ -1,25 +1,33 @@
 describe('template spec', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000/how-you-felt/entry/11')
+    cy.intercept('GET', 'http://localhost:3001/api/v1/entries', {
+      statusCode: 200,
+      fixture: 'entries.json'
+    })
+    cy.visit('http://localhost:3000/how-you-felt/entry/1')
+  })
+
+  it('Should display the journal entry page', () => {
+    cy.get('.journal-boundary').should('be.visible')
   })
 
   it('Should display the date that the selected journal entry was written as the header', () => {
-    cy.visit('https://example.cypress.io')
+    cy.get('h2').should('be.visible').contains('Apr 7, 2023')
   })
 
   it('Should display a subheading that tells the user what feeling and activity they selected on this particular day', () => {
-    cy.visit('https://example.cypress.io')
+    cy.get('h3').should('be.visible').contains('Today you were feeling anxious so you decided to Go on a long walk')
   })
 
   it('Should display the entry completed by the user on this date', () => {
-    cy.visit('https://example.cypress.io')
+    cy.get('.journal-text').should('be.visible').contains('Today I am feeling anxious because I have a big test coming up!')
   })
 
   it('Should display a button that brings the user from the entry page back to the log page', () => {
-    cy.visit('https://example.cypress.io')
+    cy.get('.uni-btn').click()
+    cy.url().should('eq', 'http://localhost:3000/how-you-felt')
+    cy.get('.journal-boundary').should('not.exist')
+    cy.get('.feelingsLog').should('be.visible')
   })
 
-  it('Should give the user the option to update their entry for that date', () => {
-    cy.visit('https://example.cypress.io')
-  })
 })
