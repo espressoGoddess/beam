802ce7db1441c63b279163e29e7ba584cc6adc1a
Katherine Blaine <blaine.katherine12@gmail.com>
2023-04-16 16:29:35 -0600

    test: update tests for feelings log DOM

--- client/cypress/e2e/log-spec.cy.js
+++ client/cypress/e2e/log-spec.cy.js
@@ -23,21 +23,21 @@ describe('Feelings log page (GET intercept)', () => {
     cy.get('tbody > :nth-child(1) > :nth-child(1)').should('be.visible').contains('Apr 7, 2023')
     cy.get('tbody > :nth-child(1) > :nth-child(2)').should('be.visible').contains('Anxious')
     cy.get('tbody > :nth-child(1) > :nth-child(3)').should('be.visible').contains('Go on a long walk')
-    cy.get('tbody > :nth-child(1) > :nth-child(4)').should('be.visible').contains('📓')
+    cy.get('tbody > :nth-child(1) > :nth-child(4)').should('be.visible')
   })
 
   it('Should display the feeling and activity the user selected on the specified date within each table column ', () => {
     cy.get('tbody > :nth-child(2) > :nth-child(1)').should('be.visible').contains('Apr 6, 2023')
     cy.get('tbody > :nth-child(2) > :nth-child(2)').should('be.visible').contains('Mad')
     cy.get('tbody > :nth-child(2) > :nth-child(3)').should('be.visible').contains("Lay on the floor")
-    cy.get('tbody > :nth-child(2) > :nth-child(4)').should('be.visible').contains('📓')
+    cy.get('tbody > :nth-child(2) > :nth-child(4)').should('be.visible')
   })
 
   it('Should display the feeling and activity the user selected on the specified date within each table column ', () => {
     cy.get('tbody > :nth-child(3) > :nth-child(1)').should('be.visible').contains('Apr 5, 2023')
     cy.get('tbody > :nth-child(3) > :nth-child(2)').should('be.visible').contains('Happy')
     cy.get('tbody > :nth-child(3) > :nth-child(3)').should('be.visible').contains('Call someone you love.')
-    cy.get('tbody > :nth-child(3) > :nth-child(4)').should('be.visible').contains('📓')
+    cy.get('tbody > :nth-child(3) > :nth-child(4)').should('be.visible')
   })
 
   it('Should have a button in the journal column that brings the user from the feelings log page to the journal entry they completed on that specific day', () => {
@@ -45,6 +45,10 @@ describe('Feelings log page (GET intercept)', () => {
     cy.url().should('eq', 'http://localhost:3000/how-you-felt/entry/1')
     cy.get('.journal-boundary').should('be.visible')
     cy.get('.feelingsLog').should('not.exist')
+
+    cy.get('h2').should('be.visible').contains('Apr 7, 2023')
+    cy.get('h3').should('be.visible').contains('Today you were feeling anxious so you decided to Go on a long walk')
+    cy.get('.journal-text').contains('Today I am feeling anxious because I have a big test coming up!')
   })
 
 })
