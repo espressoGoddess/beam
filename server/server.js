365ebf4d4e6715b6d68a1b1c59708fbfc4677fd9
Amber Shipley <ashipley2@me.com>
2023-04-17 15:32:50 -0700

    Update procfile

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const knex = require('./knex');
-const port = 3001;
+let port = process.env.PORT || 3001;
 
 app.set('port', port);
 
