dcf437ba63e0ea603f4ca2e0c66bb9a376f78985
Joe Fogiato <joe.fogiato@gmail.com>
2023-04-24 16:09:35 -0400

    Add: mobile responsiveness for the Home component

--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -16,7 +16,7 @@ const Home = () => {
         {blurb &&
           <div className='blurb-section'>
             <h2>Beam is an interactive self-care feeling tracking app.</h2>
-            <h3 className='blurb'>We created a flow intended to help you work through any kind of feeling or emotion you might be experiencing. You&apos;ll be asked to choose a feeling and reflect on it. Add anything about your experience that might help you avoid or recreate the feeling in the future (depending on the feeling). After your reflection, you&apos;ll see an offering of a few activities that might help. <br/><br/>Your entries will be available to you in the Feelings Archive for future reflection. <br/> <br/>We hope this process will help reveal useful patterns over time.</h3> 
+            <p className='blurb'>We created a flow intended to help you work through any kind of feeling or emotion you might be experiencing. You&apos;ll be asked to choose a feeling and reflect on it. Add anything about your experience that might help you avoid or recreate the feeling in the future (depending on the feeling). After your reflection, you&apos;ll see an offering of a few activities that might help. <br/><br/>Your entries will be available to you in the Feelings Archive for future reflection. <br/> <br/>We hope this process will help reveal useful patterns over time.</p> 
           </div> 
         }
       </div>
