e574669290b6fe4f67500acd83485f75b9a91c3e
Amber Shipley <ashipley2@me.com>
2023-08-14 13:40:20 -0700

    Try using alternate connection form

--- server/knexfile.js
+++ server/knexfile.js
@@ -33,7 +33,10 @@ module.exports = {
   production: {
     client: "postgresql",
     connection: {
-      connectionString: process.env.POSTGRES_URL,
+      host: process.env.POSTGRES_HOST,
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
       // ssl: {
       //   rejectUnauthorized: false
       // }
