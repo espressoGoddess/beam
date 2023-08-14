1845bbc2f01e9b27ea017983279d2d72d6ea8d16
Amber Shipley <ashipley2@me.com>
2023-08-14 13:43:14 -0700

    Revert to prior connection string, and re-add 'rejectUnauthorized' exception

--- server/knexfile.js
+++ server/knexfile.js
@@ -13,38 +13,14 @@ module.exports = {
       database: "beam_api",
     },
   },
-
-  // staging: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // },
-
   production: {
     client: "postgresql",
     connection: {
-      host: process.env.POSTGRES_HOST,
-      user: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DATABASE,
-      // ssl: {
-      //   rejectUnauthorized: false
-      // }
+      connectionString: process.env.POSTGRES_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
     },
-    // pool: {
-    //   min: 2,
-    //   max: 10,
-    // },
     migrations: {
       tableName: "knex_migrations",
     },
