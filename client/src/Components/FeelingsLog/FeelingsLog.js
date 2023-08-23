5664a485b20b9d4d346a065416632ce08fbc8814
Amber Shipley <ashipley2@me.com>
2023-08-22 17:50:34 -0700

    Fix mobile design for scrollability

--- client/src/Components/FeelingsLog/FeelingsLog.js
+++ client/src/Components/FeelingsLog/FeelingsLog.js
@@ -1,60 +1,68 @@
-import React from 'react';
-import './FeelingsLog.css';
-import { DateTime } from 'luxon';
-import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
+import React from "react";
+import "./FeelingsLog.css";
+import { DateTime } from "luxon";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
-const FeelingsLog = ({logs}) => {
+const FeelingsLog = ({ logs }) => {
   const toTitleCase = (item) => {
-    return `${ item.slice(0, 1).toUpperCase() }${ item.slice(1) }`;
-  }
+    return `${item.slice(0, 1).toUpperCase()}${item.slice(1)}`;
+  };
 
   const sorted = logs
-    .map(log => ({
+    .map((log) => ({
       ...log,
-      date: DateTime.fromFormat(log.date, 'yyyy-MM-dd'),
+      date: DateTime.fromFormat(log.date, "yyyy-MM-dd"),
     }))
     .sort((a, b) => b.date - a.date);
 
-  const  userLogs = sorted.map(log => {
-    return (<tr key={log.entry_id}>
+  const userLogs = sorted.map((log) => {
+    return (
+      <tr key={log.entry_id}>
         <td>{log.date.toLocaleString(DateTime.DATE_MED)}</td>
         <td>{toTitleCase(log.feeling)}</td>
-        <td className='truncate'>{log.activity}</td>
-        <td className='journal'>{<Link to={`/how-you-felt/entry/${log.entry_id}`}><i className="fa-solid fa-book"></i></Link>}</td>
-      </tr>);
+        <td className="truncate">{log.activity}</td>
+        <td className="journal">
+          {
+            <Link to={`/how-you-felt/entry/${log.entry_id}`}>
+              <i className="fa-solid fa-book"></i>
+            </Link>
+          }
+        </td>
+      </tr>
+    );
   });
 
   return (
-    <section className='feelingsLog'>
+    <section className="feelingsLog">
       <table>
         <thead>
           <tr>
             <th>Date</th>
-            <th className='feeling-head'>Feeling</th>
-            <th className='activity-head'>Activity</th>
+            <th className="feeling-head">Feeling</th>
+            <th className="activity-head">Activity</th>
             <th>Journal</th>
           </tr>
         </thead>
-        <tbody>
-        {userLogs}
-        </tbody>
+        <tbody>{userLogs}</tbody>
       </table>
     </section>
   );
-}
+};
 
 export default FeelingsLog;
 
 FeelingsLog.propTypes = {
-  logs: PropTypes.arrayOf(PropTypes.shape({
-    entry_id: PropTypes.number.isRequired,
-    user_id: PropTypes.string,
-    date: PropTypes.string.isRequired,
-    feeling: PropTypes.string.isRequired,
-    journal_entry: PropTypes.string.isRequired,
-    created_at: PropTypes.string,
-    updated_at: PropTypes.string,
-    activity: PropTypes.string.isRequired
-  }))
-}
+  logs: PropTypes.arrayOf(
+    PropTypes.shape({
+      entry_id: PropTypes.number.isRequired,
+      user_id: PropTypes.string,
+      date: PropTypes.string.isRequired,
+      feeling: PropTypes.string.isRequired,
+      journal_entry: PropTypes.string.isRequired,
+      created_at: PropTypes.string,
+      updated_at: PropTypes.string,
+      activity: PropTypes.string.isRequired,
+    })
+  ),
+};
